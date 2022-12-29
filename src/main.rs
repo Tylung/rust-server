72c2ccc115f830273facf0204f6d5d59d650189e
jona <jonaenglish64@gmail.com>
2022-12-29 13:45:13 -0600

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 // use rocket_dyn_templates::{Template, context};
-use rocket::serde::{Deserialize, json::Json};
+use rocket::serde::{json::Json, Deserialize};
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Persona<'r> {
     nombre: &'r str,
-    edad: u8
+    edad: u8,
 }
 
 #[get("/")]
@@ -17,31 +18,37 @@ fn index() -> &'static str {
     "Hola Mundo"
 }
 
-#[post("/api", data = "<persona>" )]
+#[post("/api", data = "<persona>")]
 fn api(persona: Json<Persona<'_>>) -> String {
-
-
     let nombre = persona.nombre;
-    let edad  = persona.edad;
+    let edad = persona.edad;
+    let mut message = format!("Hola Api");
 
-    if  nombre.len() < 1 && edad.to_string().len() < 1 {
-        return format!("Test")
+    if nombre.len() < 1 && edad.to_string().len() < 1 {
+        message = format!("Test");
     }
-    if  nombre != ""  && edad > 18 {
-        return format!("Hola {}, tu edad es {}, así que eres mayor de edad", nombre, edad)
-    } if nombre != ""  && edad < 18 {
-        return format!("Hola {}, tu edad es {}, así que eres menor de edad", nombre, edad)
-    } else {
-        return format!("Hola Api")
+    if nombre != "" && edad > 18 {
+        message = format!(
+            "Hola {}, tu edad es {}, así que eres mayor de edad",
+            nombre, edad
+        );
     }
-    // format!("{} y {}", persona.edad, persona.nombre)
+    if nombre != "" && edad < 18 {
+        message = format!(
+            "Hola {}, tu edad es {}, así que eres menor de edad",
+            nombre, edad
+        );
+    }
+
+    println!("{}", message);
+
+    return message;
 }
 
 #[get("/")]
 fn profile() -> &'static str {
     "Profile!"
 }
-
 
 #[post("/")]
 fn create_profile() -> &'static str {
@@ -59,32 +66,21 @@ fn delete_profile() -> &'static str {
 }
 
 #[catch(404)]
-fn not_found() ->  &'static str {
+fn not_found() -> &'static str {
     // Template::render("notFound", context! {
     //     msg: "Not Found"
     // })
     "Not found"
 }
 
-
-
-
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .register("/", catchers![not_found])
         .mount("/", routes![index, api])
-        .mount("/profile", routes![profile, create_profile, update_profile, delete_profile])
-        // .attach(Template::fairing())
+        .mount(
+            "/profile",
+            routes![profile, create_profile, update_profile, delete_profile],
+        )
+    // .attach(Template::fairing())
 }
-
-
-
-
-
-
-
-
-
-
-
